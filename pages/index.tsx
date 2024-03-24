7ec9a8e2f3a2aeec52caa741fb4912d3b7f8d332
Joshua Gagnier <gagnierjoshua@gmail.com>
2024-03-24 16:23:57 -0700

    navfixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,6 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 const HomePage = () => {
 
-
-
 const [nav, setNav]= useState(false);
 const openNav = () => setNav(true); 
 const closeNav=() => setNav(false);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+
+const config: Config={
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite/**/*.{js,ts,jsx,tsx,mdx}",
     "node_modules/flowbite-react/lib/esm/**/*.{js,ts,jsx,tsx,mdx}",
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+
   theme: {
     extend: {
       fontFamily: {
+        logo: ["Oleo Script", "system-ui"],
         primary: ["Hedvig Letters Serif"]
       },
     },
   },
   plugins: [ require('flowbite/plugin')],
-}
+};
+export default config;
