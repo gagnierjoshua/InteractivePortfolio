dd17ed2fb18360bf7ba8c76c9eb51edec40d7434
Joshua Gagnier <gagnierjoshua@gmail.com>
2024-03-24 17:07:00 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        logo: ["Oleo Script", "system-ui"],
-        primary: ["Poppins", "sans-serif"],
+        primary: ["Hedvig Letters Serif"]
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [ require('flowbite/plugin')],
+}
