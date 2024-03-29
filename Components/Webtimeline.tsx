237480936fd4e62408a7c5d192e5188a48a9f55c
Joshua Gagnier <gagnierjoshua@gmail.com>
2024-03-28 19:47:31 -0700

    Update Webtimeline.tsx

--- Components/Webtimeline.tsx
+++ Components/Webtimeline.tsx
@@ -27,9 +27,11 @@ function Webtimeline() {
 
 
     <Card className="max-w-sm max-w-lg max-w-lg-[mb-8] max-h-[650px]" >
+    <div className="flex items-center justify-center"> 
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Project #1 Title
       </h5>
+      </div>
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-85">
       <Carousel>
         <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
@@ -40,76 +42,17 @@ function Webtimeline() {
       </Carousel>
       </div>
       <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+       This is the first web app that I am looking to develop and flush out and put in my portfolio.
+       I will add more cards like this below for future projects. 
       </p>
-      <Button>
-        Read more
-        <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Button>
+      <div className="flex items-center justify-center"> 
+      <Button.Group>
+      <Button color="gray">View Live</Button>
+      <Button color="gray">GitHub</Button>
+    </Button.Group>
+    </div>
     </Card>
 
-    <Card className="max-w-sm max-w-lg max-w-lg-[mb-8] max-h-[650px]" >
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Project #1 Title
-      </h5>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-85">
-      <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-      </Carousel>
-      </div>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-      </p>
-      <Button>
-        Read more
-        <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Button>
-    </Card>
-
-
-    <Card className="max-w-sm max-w-lg max-w-lg-[mb-8] max-h-[650px]" >
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Project #1 Title
-      </h5>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-85">
-      <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-      </Carousel>
-      </div>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-      </p>
-      <Button>
-        Read more
-        <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Button>
-    </Card>
 
   
       </div>
