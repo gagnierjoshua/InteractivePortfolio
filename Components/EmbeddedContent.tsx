8704ef0fece4df1acb9f5d34d91f98031ad78932
Joshua Gagnier <gagnierjoshua@gmail.com>
2024-04-14 07:31:12 -0700

    embedded updates

--- Components/EmbeddedContent.tsx
+++ Components/EmbeddedContent.tsx
@@ -3,7 +3,7 @@ import { Tabs } from "flowbite-react";
 import { HiGlobeAlt } from "react-icons/hi";
 import { HiAdjustments, HiOutlineDocumentText ,HiClipboardList, HiOutlineBeaker ,HiOutlineSearchCircle, HiOutlineWifi, HiCog, HiUserCircle } from "react-icons/hi";
 import { MdDashboard } from "react-icons/md";
-
+import { Table } from "flowbite-react";
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Grommet, Box, Diagram, Stack } from "grommet/components";
@@ -17,23 +17,49 @@ const EmbeddedContent = () => {
 
         <div className="overflow-x-auto">
 
-            <div>
+            <div className="text-center text-3xl">
+              <br />
+        <h1> Embedded Engineering</h1>
+        <br />
 
 
-
-
-        <p>hi</p>
+      
 
             </div>
         <Tabs aria-label="Full width tabs" style="fullWidth">
           <Tabs.Item active title="Overview" icon={HiOutlineDocumentText }>
+                   
+
+
+
+<div className=" grid-cols-1 mx-auto grid lg:grid-cols-2">
+
+
+           <div className="text-center items-center px-10 w-80% ">
             
             
+            Since 2018 I've completed personal projects and supported students solving problems competitively with great success utilizing the engineering design process primarily with embedded devices. 
+            <br />
+            <br />
+            I've worked with students to identify problems, select platforms, sensors, and devices, and develop prototypes for an extremely wide range of solutions.
+            <br />
+            <br />
+            As a result I have amassed hundreds of sensors and thousands of devices to integrate with 
+            the 80+ boards I have in my collection.
+            <br />
+            <br />
+
+            I am at a point where given any problem I can identify a platform, sensors, and devices to build a functioning protoype. 
+            
+
+            </div>
 
 
 
+            <div className="w-[80%] width-[650px]  justify-center items-center">
+  <div className="flex justify-center items-center">
 
-
+        
           <Stack guidingChild={1}>
   <Diagram
 
@@ -68,14 +94,15 @@ const EmbeddedContent = () => {
       },
     ]}
   />
+  
   <Box>
     <Box direction="row" margin={{ bottom: 'large' }} >
         
       <Box id="1" margin="small" pad="small" background="light-4" >
-        box 1 text test qwadjkaksdkahjsdk  ashdjads  asdjnaksdadnsjk
+        Problem Identification (What is the problem trying to be solved?)
         </Box>
       <Box id="2" margin="small" pad="small" background="light-4" >
-        Box 2 text testad asdaasd asdasd asdassdasdasd
+        Platform Identification (Arduino, Raspberry Pi, ESP, etc.)
         
         </Box>
     </Box>
@@ -83,14 +110,16 @@ const EmbeddedContent = () => {
     <Box direction="row"  margin={{ bottom: 'large' }}>
       <Box id="3" margin="small" pad="small" background="light-4" >
      
-      box3 test
+      Sensor and Device Selection (Servo, Motor, LEDs, Relay)
       </Box>
       <Box id="4" margin="small" pad="small" background="light-4" >
-      box4 test
+      Prototype Development & Testing
       </Box>
     </Box>
   </Box>
 </Stack>
+</div>
+  </div>
 
 
 
@@ -99,20 +128,54 @@ const EmbeddedContent = () => {
 
 
 
-
-
-
-            
-            
-            
-            This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
-            Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-            control the content visibility and styling.
+            </div>
           </Tabs.Item>
           <Tabs.Item title="IoT & BT" icon={HiOutlineWifi}>
-            This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
-            Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-            control the content visibility and styling.
+          
+
+          <div className="flex justify-between">
+          <div className="overflow-x-auto">
+
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>Device Name</Table.HeadCell>
+          <Table.HeadCell>Utilization</Table.HeadCell>
+    
+        </Table.Head>
+        <Table.Body className="">
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Apple MacBook Pro 17"'}
+            </Table.Cell>
+            <Table.Cell>Sliver</Table.Cell>
+       
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Microsoft Surface Pro
+            </Table.Cell>
+            <Table.Cell>White</Table.Cell>
+         
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+            <Table.Cell>Black</Table.Cell>
+       
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
+    <div className="text-center">
+    <p>text</p>
+
+
+    </div>
+    </div>
+
+
+
+
+            
           </Tabs.Item>
           <Tabs.Item title="Mechanical" icon={HiCog}>
             This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
